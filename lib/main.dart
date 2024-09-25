db6bd2d2ca20c9a4c0a1c432ff44c080405ba846
Poczik László <pocziklaszlo@hmzrinyi.hu>
2024-09-25 14:40:30 +0200

    stateful widget példa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+// Stateless widget 1 példa
+/*
 class ColoredCardWidget extends StatelessWidget {
   final Color color;
   final Widget child;
@@ -16,6 +17,34 @@ class ColoredCardWidget extends StatelessWidget {
       alignment: Alignment.center,
       padding: const EdgeInsets.all(16),
       child: child,
+    );
+  }
+}
+*/
+
+// Stateful widget 2 példa
+class CounterWidget extends StatefulWidget {
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          child: Text('Press me'),
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+        ),
+        Text('Counter value: $count'),
+      ],
     );
   }
 }
